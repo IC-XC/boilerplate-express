a42c4b01bb787aa89687eb69f75e14e94fbe4827
Per <86513555+IC-XC@users.noreply.github.com>
2021-11-21 20:06:37 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello World");
-
-app.get("/", function(req, res) {
-  res.send("Hello Express");
-});
-
-app.get('/', (req, res) => {
-  response.sendFile(__dirname+"/views/index.html");
-});
 
 // Normal usage
 app.use(express.static(__dirname + "/public"));
