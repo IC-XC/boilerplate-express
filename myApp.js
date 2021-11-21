6d4116ecd03b4e554ea226aa1712d616c3f87d19
Per <86513555+IC-XC@users.noreply.github.com>
2021-11-21 17:29:04 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello Express');
+console.log("Hello World");
+
+app.get("/", function(req, res) {
+  res.send("Hello Express");
 });
+
+
 
 app.gett('/', function (req, res) {
   res.sendFile('__dirname + relativePath/file.ext');
