0365017f6190b2505c0ec70eeb88244c1b687354
Per <86513555+IC-XC@users.noreply.github.com>
2021-11-21 20:00:32 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -11,6 +11,12 @@ app.get('/', (req, res) => {
   response.sendFile(__dirname+"/views/index.html");
 });
 
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
 
 
 
